182588dbc4342642c75acac6de943e4457d1fc74
Gonzalo <gmarquez@mexerp.com>
2017-05-10 11:01:37 -0500

    added expansion panels to home view to list posts

--- src/components/home.js
+++ src/components/home.js
@@ -1,11 +1,44 @@
 import React, { Component } from 'react';
-import '../styles/home.css'
+import AppBar from 'material-ui/AppBar';
+import '../styles/home.css';
 
 class Home extends Component {
   render() {
     return (
       <div className="main-box">
-
+        <details>
+            <summary>
+                <ul>
+                  <li>Meal Preference <span>Optional</span></li>
+                    <li className="titleValue"></li>
+                </ul>
+            </summary>
+            <div className="content">
+                <p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.</p>
+            </div>
+        </details>
+        <details>
+            <summary>
+                <ul>
+                  <li>Meal Preference <span>Optional</span></li>
+                    <li className="titleValue"></li>
+                </ul>
+            </summary>
+            <div className="content">
+                <p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.</p>
+            </div>
+        </details>
+        <details>
+            <summary>
+                <ul>
+                  <li>Meal Preference <span>Optional</span></li>
+                    <li className="titleValue"></li>
+                </ul>
+            </summary>
+            <div className="content">
+                <p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.</p>
+            </div>
+        </details>
       </div>
     )
   }
--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import { HashRouter } from 'react-router-dom';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import injectTapEventPlugin from 'react-tap-event-plugin';
 import App from './components/app';
+injectTapEventPlugin();
 
 ReactDOM.render(
   (
     <HashRouter>
-      <App />
+      <MuiThemeProvider>
+        <App />
+      </MuiThemeProvider>
     </HashRouter>
   ),
   document.getElementById('container')
